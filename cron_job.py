dd63a7ceeae7d8c1100900ff81f074d5e5a6185f
L-Phuong <phuonglehp2712@gmail.com>
2024-12-11 09:47:06 +0700

    Update cron_job.py

--- cron_job.py
+++ cron_job.py
@@ -2,8 +2,8 @@ import schedule
 import os
 
 def my_job():
-    # os.system("crawl_main.py")
-    print("helu")
+    os.system("crawl_main.py")
+
 
 # schedule.every(15).minutes.do(my_job)
 # schedule.every().day.at("10:30").do(my_job)
