4c0ce3bc128cc9e19e473c5aef5a15168bafdf0d
L-Phuong <phuonglehp2712@gmail.com>
2024-12-14 08:35:04 +0700

    tu_dong_hoa

--- cron_job.py
+++ cron_job.py
@@ -2,12 +2,12 @@ import schedule
 import os
 
 def my_job():
-    os.system("crawl_main.py")
+    os.system("VietComBankData.py")
 
 
-# schedule.every(15).minutes.do(my_job)
+schedule.every(20).minutes.do(my_job)
 # schedule.every().day.at("10:30").do(my_job)
-schedule.every(5).seconds.do(my_job)
+# schedule.every(20).seconds.do(my_job)
 
 while True:
     schedule.run_pending()
